7daa93643f260061480b87be294588df37338ac1
Tolga Pirtürk <tpirturk@gmail.com>
2022-12-02 22:51:29 +0300

    Create TitlePreviewViewController.
    Create TitlePreviewViewController with title, overview, download button and webView. Add constraints for views. Create TitlePreviewViewModel. By using view model fetch title, overview and video from YouTube API for the TitlePreviewViewController. Add a protocol called CollectionViewTableViewCellDelegate. Make HomeViewController conform to this protocol. When any item on home page tapped, display preview view by the delegate method.

---
+++ Netflix Clone/Netflix Clone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix Clone
+//
+//  Created by Tolga PIRTURK on 2.12.2022.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeView: VideoElement
+    let titleOverview: String
+}
